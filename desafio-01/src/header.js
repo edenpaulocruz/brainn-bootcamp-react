487fa1e124fef4554f869b45910a41c74511a746
edenpaulocruz <edenpaulo@gmail.com>
2021-09-07 13:38:27 -0300

    Desafio #01 - Criação dos componentes de título (h1 até o h6)

--- desafio-01/src/header.js
+++ desafio-01/src/header.js
@@ -1,10 +1,10 @@
-import Title from "./title"
+import * as title from "./title"
 import Menu from "./menu"
 
 function Header () {
   return (
     <header className='header'>
-      <Title />
+      <title.Heading1>App</title.Heading1>
       <Menu />
     </header>
   )
--- desafio-01/src/title.js
+++ desafio-01/src/title.js
@@ -1,7 +1,36 @@
-function Title () {
+function Heading1 ({ children }) {
   return (
-    <h1>App</h1>
+    <h1>{children}</h1>
   )
 }
 
-export default Title
+function Heading2 ({ children }) {
+  return (
+    <h2>{children}</h2>
+  )
+}
+
+function Heading3 ({ children }) {
+  return (
+    <h3>{children}</h3>
+  )
+}
+
+function Heading4 ({ children }) {
+  return (
+    <h4>{children}</h4>
+  )
+}
+
+function Heading5 ({ children }) {
+  return (
+    <h5>{children}</h5>
+  )
+}
+
+function Heading6 ({ children }) {
+  return (
+    <h6>{children}</h6>
+  )
+}
+export { Heading1, Heading2, Heading3, Heading4, Heading5, Heading6 }
