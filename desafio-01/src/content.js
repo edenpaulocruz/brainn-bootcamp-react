455f6a08faeab195a5734f8e0f48d39553ee3a54
edenpaulocruz <edenpaulo@gmail.com>
2021-09-07 14:08:02 -0300

    Desafio #01 - Criação do componente botão com 2 variantes

--- desafio-01/src/content.js
+++ desafio-01/src/content.js
@@ -1,7 +1,12 @@
+import * as title from './title'
+import Button from './button'
+
 function Content () {
   return (
     <main className='main'>
-      Conteúdo
+      <title.Heading2>Conteúdo</title.Heading2>
+      <Button kind='primary' text='Primary' />
+      <Button kind='secondary' text='Secondary' />
     </main>
   )
 }
