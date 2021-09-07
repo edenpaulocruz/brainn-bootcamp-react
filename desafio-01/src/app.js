ff0cefe5e8161be3736fcc1c8f27e7ae7a096a40
edenpaulocruz <edenpaulo@gmail.com>
2021-09-06 23:26:34 -0300

    Desafio #01 - Criação da estrutura básica do site

--- desafio-01/src/app.js
+++ desafio-01/src/app.js
@@ -1,6 +1,53 @@
 function App () {
   return (
-    <h1>App</h1>
+    <>
+      <h1>App</h1>
+      <Header />
+      <Menu />
+      <Content />
+      <Sidebar />
+      <Footer />
+    </>
+  )
+}
+
+function Header () {
+  return (
+    <header>
+      Header
+    </header>
+  )
+}
+
+function Menu () {
+  return (
+    <nav>
+      Menu
+    </nav>
+  )
+}
+
+function Content () {
+  return (
+    <main>
+      Conteúdo
+    </main>
+  )
+}
+
+function Sidebar () {
+  return (
+    <aside>
+      Sidebar
+    </aside>
+  )
+}
+
+function Footer () {
+  return (
+    <footer>
+      Footer
+    </footer>
   )
 }
 
