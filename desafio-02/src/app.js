77cad7a43dc501e3a13be4bd86e8bfae29da130c
edenpaulocruz <edenpaulo@gmail.com>
2021-09-09 10:47:47 -0300

    Desafio #02 - Exibindo primeiro membro da equipe em Content

--- desafio-02/src/app.js
+++ desafio-02/src/app.js
@@ -1,51 +1,66 @@
+import { useState } from 'react'
 import { Header } from './header'
 import { Content } from './content'
 import { Sidebar } from './sidebar'
 import { Footer } from './footer'
+import * as Title from './title'
 
-const team = [
+const members = [
   {
     id: 1,
     title: 'Fernando Daciuk',
-    content: { 
-      avatar: 'https://pbs.twimg.com/profile_images/1415288716415877121/zK7lC5Ns.jpg',
-      role: 'Tech Lead | Software Developer',
-      description: 'Teacher and web developer, focused on JavaScript, both on client and server (with Node.js).',
-      experience: [
-        {
-          role: 'Founder & Teacher',
-          company: 'queroser.ninja'
-        },
-        {
-          role: 'Founder & CTO',
-          company: 'Lukin Co.'
-        }
-      ]
-    }
+    content:
+      <>
+        <Title.Heading2>Tech Lead | Software Developer</Title.Heading2>
+        <img src='https://pbs.twimg.com/profile_images/1415288716415877121/zK7lC5Ns.jpg' alt='Ferando Daciuk' />
+        <p>Teacher and web developer, focused on JavaScript, both on client and server (with Node.js).</p>
+        <Title.Heading3>Experience</Title.Heading3>
+        <ul>
+          <li className='experience'>
+            <Title.Heading4>Founder &amp; Teacher</Title.Heading4>
+            queroser.ninja
+          </li>
+          <li className='experience'>
+            <Title.Heading4>Founder &amp; CTO</Title.Heading4>
+            Lukin Co.
+          </li>
+        </ul>
+      </>,
   },
   {
     id: 2,
     title: 'Diego Moretti',
-    content: {
-      avatar: 'https://pbs.twimg.com/profile_images/1432437836800794636/_TEdKmBy_400x400.jpg',
-      role: 'Developer Relations',
-      description: 'Tech, Web & Community specialist.',
-      experience: [
-        {
-          role: 'Co-Creator & Member',
-          company: 'Front-End Brasil'
-        },
-        {
-          role: 'Founder & Organizer',
-          company: 'Front in Floripa'
-        }
-      ]
-    }
+    content:
+      <>
+        <Title.Heading3>Developer Relations</Title.Heading3>
+        <img src='https://pbs.twimg.com/profile_images/1432437836800794636/_TEdKmBy_400x400.jpg' alt='Diego Moretti'/>
+        <p>Tech, Web &amp; Community specialist.</p>
+        <Title.Heading4>Experience</Title.Heading4>
+        <ul>
+          <li className="experience">
+            <Title.Heading4>Co-Creator &amp; Member</Title.Heading4>
+            Front-End Brasil
+          </li>
+          <li className="experience">
+            <Title.Heading4>Founder &amp; Organizer</Title.Heading4>
+            Front in Floripa
+          </li>
+        </ul>
+      </>
   },
-  {
+  /*{
     id: 3,
     title: 'Adeonir Kohl',
-    content: {
+    content:
+    <>
+      <Title.Heading3></Title.Heading3>
+      <p></p>
+      <Title.Heading4>Experience</Title.Heading4>
+      <ul>
+        <li className="experience"><span></span></li>
+        <li className="experience"><span></span></li>
+      </ul>
+    </> {
       avatar: 'https://pbs.twimg.com/profile_images/1369253967939657729/q6bEIJ7A_400x400.jpg',
       role: 'Frontend Developer',
       description: 'Web developer with background in Graphic Design and Photography.',
@@ -64,7 +79,16 @@ const team = [
   {
     id: 4,
     title: 'Amorésio de Souza',
-    content: {
+    content:
+    <>
+      <Title.Heading3></Title.Heading3>
+      <p></p>
+      <Title.Heading4>Experience</Title.Heading4>
+      <ul>
+        <li className="experience"><span></span></li>
+        <li className="experience"><span></span></li>
+      </ul>
+    </> {
       avatar: 'https://pbs.twimg.com/profile_images/1419345932177510403/NQEr_zI6_400x400.jpg',
       role: 'Backend Product Developer',
       description: 'Computer Science Academic. Passionate about programming.',
@@ -83,7 +107,16 @@ const team = [
   {
     id: 5,
     title: 'Pedro Azevedo',
-    content: {
+    content:
+    <>
+      <Title.Heading3></Title.Heading3>
+      <p></p>
+      <Title.Heading4>Experience</Title.Heading4>
+      <ul>
+        <li className="experience"><span></span></li>
+        <li className="experience"><span></span></li>
+      </ul>
+    </> {
       avatar: 'https://pbs.twimg.com/profile_images/1392878203606380545/ZYa3HMEt_400x400.jpg',
       role: 'Front-End Developer | JavaScript | React Native | React',
       description: 'Always willing to learn new technologies to improve my knowledge skills.',
@@ -98,15 +131,21 @@ const team = [
         }
       ]
     }
-  }
+  }*/
 ]
 
 function App () {
+  const [title, setTitle] = useState(members[0].title)
+  const [content, setText] = useState(members[0].content)
+
   return (
     <div className='app'>
       <Header />
-      <Content />
-      <Sidebar content={team}/>
+      <Content 
+        title={title}
+        content={content}
+      />
+      <Sidebar sidebarContent={members} />
       <Footer />
     </div>
   )
--- desafio-02/src/content.js
+++ desafio-02/src/content.js
@@ -1,18 +1,12 @@
-import * as title from './title'
-import { Button } from './button'
+import { Heading1 } from './title'
 
-function Content () {
+function Content ({title, content}) {
   return (
     <main className='main'>
-      <title.Heading2>Conteúdo</title.Heading2>
-      <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione doloremque odio facilis officia deleniti quod sequi fuga et porro molestiae modi aperiam repellendus nesciunt dolorem, quisquam voluptatibus sapiente eaque laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione doloremque odio facilis officia deleniti quod sequi fuga et porro molestiae modi aperiam repellendus nesciunt dolorem, quisquam voluptatibus sapiente eaque laboriosam.</p>
-      <title.Heading3>Conteúdo 2</title.Heading3>
-      <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione doloremque odio facilis officia deleniti quod sequi fuga et porro molestiae modi aperiam repellendus nesciunt dolorem, quisquam voluptatibus sapiente eaque laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione doloremque odio facilis officia deleniti quod sequi fuga et porro molestiae modi aperiam repellendus nesciunt dolorem, quisquam voluptatibus sapiente eaque laboriosam.</p>
-      <title.Heading4>Conteúdo 3</title.Heading4>
-      <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione doloremque odio facilis officia deleniti quod sequi fuga et porro molestiae modi aperiam repellendus nesciunt dolorem, quisquam voluptatibus sapiente eaque laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione doloremque odio facilis officia deleniti quod sequi fuga et porro molestiae modi aperiam repellendus nesciunt dolorem, quisquam voluptatibus sapiente eaque laboriosam.</p>
-      <title.Heading5>Conteúdo 4</title.Heading5>
-      <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione doloremque odio facilis officia deleniti quod sequi fuga et porro molestiae modi aperiam repellendus nesciunt dolorem, quisquam voluptatibus sapiente eaque laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione doloremque odio facilis officia deleniti quod sequi fuga et porro molestiae modi aperiam repellendus nesciunt dolorem, quisquam voluptatibus sapiente eaque laboriosam.</p>
-      <Button kind='primary' text='Diga olá' />
+      <>
+        <Heading1>{title}</Heading1>
+        <>{content}</>
+      </>
     </main>
   )
 }
